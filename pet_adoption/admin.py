f577f9c01fa8b726e2b00659f80de7532ac76de0
Aditya Agarwal <adityaagarwal201203@gmail.com>
2025-10-29 00:22:50 +0530

    add admin.py code for Doctor model

--- pet_adoption/admin.py
+++ pet_adoption/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post, Contact
+from .models import Post, Contact, Doctor
 
 class ContactModelAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone_number')
@@ -7,3 +7,10 @@ class ContactModelAdmin(admin.ModelAdmin):
 # Register your model with the custom ModelAdmin class
 admin.site.register(Contact, ContactModelAdmin)
 admin.site.register(Post)
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('account', 'specialization', 'clinic_name', 'pincode', 'account__phone_number')
+    search_fields = ('account__first_name', 'account__last_name', 'clinic_name', 'pincode', 'specialization', 'account__phone_number')
+    list_filter = ('pincode', 'specialization')
